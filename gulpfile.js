e829e3bc1cb10098ef0a49930d869e08fe3fb8f7
CMrookie <944727074@qq.com>
2016-12-17 20:41:41 +0800

    add gulpfile and package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,34 @@
 /**
  * Created by chenyansong on 2016/12/17.
  */
-// var gulp  = require('gulp'),
-//     vulcanize = require('vulcanize'),
-//     polymerServer = require('polymer-cli')();
-//
-// gulp.task('default',() => {
-//     // console.log(polymerServer);
-//     polymerServer.forEach((item, index, arr) => {
-//         console.log(item);
-//     })
-// })
+let gulp = require('gulp'),
+    path = require('path'),
+    del = require('del'),
+    vulcanize = require('gulp-vulcanize');
+
+let filePath = {
+    root: path.normalize('./src'),
+    src: path.normalize('src/**/*.html'),
+    dist: path.normalize('./dist')
+};
+
+gulp.task('watch', () => {
+    "use strict";
+    gulp.watch([filePath.root, filePath.src], () => {
+        try {
+            gulp.src(filePath.src)
+                .pipe(vulcanize({
+                    abspath: '',
+                    excludes: [],
+                    stripExcludes: false,
+                    inlineScripts: false
+                })).pipe(gulp.dest(filePath.dist));
+        }
+        catch (err){
+            console.log(err);
+        }
+    })
+});
+
+gulp.task('default', ['watch']);
 
