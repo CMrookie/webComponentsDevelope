bbe8a08404d970e098f01e3bfa1edae52f27a981
CMrookie <944727074@qq.com>
2016-12-17 16:29:54 +0800

    add gulpfile and package.json

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+/**
+ * Created by chenyansong on 2016/12/17.
+ */
+// var gulp  = require('gulp'),
+//     vulcanize = require('vulcanize'),
+//     polymerServer = require('polymer-cli')();
+//
+// gulp.task('default',() => {
+//     // console.log(polymerServer);
+//     polymerServer.forEach((item, index, arr) => {
+//         console.log(item);
+//     })
+// })
+
